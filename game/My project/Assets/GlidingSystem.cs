21f75b356d6b82a837558298bcfa3e7c607f728a
NonoNoom <nvitner@outlook.com>
2023-10-31 15:54:47 +0100

    Gliding??

--- game/My project/Assets/GlidingSystem.cs
+++ game/My project/Assets/GlidingSystem.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GlidingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float BaseSpeed = 30f;
+    [SerializeField] private float MaxThrustSpeed;
+    [SerializeField] private float MinThrustSpeed;
+    private float CurrentThrustSpeed;
+    private Rigidbody Rb;
+
+
+    private void Start(){
+        Rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate(){
+        GlidingMovement();
+    }
+
+    private void Update(){
+
+    }
+
+    private void GlidingMovement(){
+        Rb.AddRelativeForce(-Vector3.forward * BaseSpeed);
+    }
+
+    private void ManageRotation(){
+
     }
 }
