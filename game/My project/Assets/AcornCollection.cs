5947863d21ac2a2650ae57c30c563eb83a02a126
NonoNoom <148919764+NonoNoom@users.noreply.github.com>
2023-11-07 17:56:17 +0100

    Collecting Acorn

---
+++ game/My project/Assets/AcornCollection.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcornCollection : MonoBehaviour
+{
+    private int Acorn = 0;
+
+    public TMPro.TextMeshProUGUI acornText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Acorn")
+        {
+            Acorn++;
+            acornText.text = "Acorn: " + Acorn.ToString();
+            Debug.Log(Acorn);
+            Destroy(other.gameObject);
+        }
+    }
+}
